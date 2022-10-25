60906411c48e22d588b9b89bf236a8231914d48b
hamztouiz <71287875+hamztouiz@users.noreply.github.com>
2022-10-25 15:17:03 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,9 +2,8 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
- let params = _req.searchParams;
- let name = params.get('text'); // is the string "Jonathan Smith".
- return new Response(name);
+
+ return new Response(_req.Params['text']);
 }
 
 serve(handler);
