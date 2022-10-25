f8044e17227dd8629ffcb94a7c53760fc6cc419b
hamztouiz <71287875+hamztouiz@users.noreply.github.com>
2022-10-25 15:27:23 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
- let tet::string = _req.body;
+ let tet::string = _req.body ;
  return new Response(tet.split('&'));
 }
 
