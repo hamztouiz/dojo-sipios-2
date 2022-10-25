0840749ecaa1723a84733693ad1e4ac084e5befb
hamztouiz <71287875+hamztouiz@users.noreply.github.com>
2022-10-25 14:57:27 +0200

    add req display

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-  return new Response("Hello World");
+  console.log(_req)
+  return new Response(_req.url);
 }
 
 serve(handler);
